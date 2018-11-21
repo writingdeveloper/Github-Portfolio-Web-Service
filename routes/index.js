2a1d0066b445b7c2f74fb740d1a7980cd97775bf
sangumee <super2451894@gmail.com>
2018-11-21 16:50:53 +0900

    Update index.js
    - Query Fix to shorter code
    - Add Comments
    - Remove Useless Codes

--- routes/index.js
+++ routes/index.js
@@ -7,11 +7,40 @@ let request = require("request");
 // DB Import
 let db = require("../lib/db");
 
-// DB Connection Check
-// db.connect(function(err) {
-//   if (err) throw err;
-//   console.log("Connected!");
-// });
+// Parse DATA
+const fieldOrder = [
+  "login",
+  "id",
+  "node_id",
+  "avatar_url",
+  "gravatar_id",
+  "url",
+  "html_url",
+  "followers_url",
+  "following_url",
+  "gists_url",
+  "starred_url",
+  "subscriptions_url",
+  "organizations_url",
+  "repos_url",
+  "events_url",
+  "received_events_url",
+  "type",
+  "site_admin",
+  "name",
+  "company",
+  "blog",
+  "location",
+  "email",
+  "hireable",
+  "bio",
+  "public_repos",
+  "public_gists",
+  "followers",
+  "following",
+  "created_at",
+  "updated_at"
+];
 
 // Routes to portFolioData.js
 let portfolioDataRouter = require("./portfolioData");
@@ -32,15 +61,12 @@ router.use(favicon(path.join(__dirname, "../public/images", "favicon.ico")));
 router.get("/", function(req, res, next) {
   // Parse LinkedIn Profile Image
   let id = req.body.id;
-
   // Log Data
   db.query(`SELECT * FROM user group by login`, function(error, data) {
     //Log Error
     if (error) {
       console.log(error);
     }
-    console.log(data);
-
     // console.log(data);
     res.render("main", {
       dataarray: data
@@ -48,94 +74,47 @@ router.get("/", function(req, res, next) {
   });
 });
 
+/* POST User Save in MySQL DB */
 router.post("/user", function(req, res, next) {
+  // id value is from PUG form
   let id = req.body.id;
   let githubAPI = "https://api.github.com/users/";
 
-  let options = {
+  // User Information API Option Set
+  let userOptions = {
     url: githubAPI + id,
     headers: {
       "User-Agent": "request"
     }
   };
-  console.log(id);
-  request(options, function(error, response, data) {
+
+  // User Repository API Option Set
+  let repositoryOptions = {
+    url: githubAPI + id + "repos",
+    headers: {
+      "User-Agent": "request"
+    }
+  };
+
+  // User Information API Process
+  request(userOptions, function(error, response, data) {
     if (error) {
       throw error;
     }
+    // result have JSON User Data
     let result = JSON.parse(data);
-
-    let nick = result.login;
-    let id = result.id;
-    let node_id = result.node_id;
-    let avatar_url = result.avatar_url;
-    let gravatar_id = result.gravatar_id;
-    let url = result.url;
-    let html_url = result.html_url;
-    let followers_url = result.followers_url;
-    let following_url = result.following_url;
-    let gists_url = result.gists_url;
-    let starred_url = result.starred_url;
-    let subscriptions_url = result.subscriptions_url;
-    let organizations_url = result.organizations_url;
-    let repos_url = result.repos_url;
-    let events_url = result.events_url;
-    let received_events_url = result.received_events_url;
-    let type = result.type;
-    let site_admin = result.site_admin;
-    let name = result.name;
-    let company = result.company;
-    let blog = result.blog;
-    let location = result.location;
-    let email = result.email;
-    let hireable = result.hireable;
-    let bio = result.bio;
-    let public_repos = result.public_repos;
-    let public_gists = result.public_gists;
-    let followers = result.followers;
-    let following = result.following;
-    let created_at = result.created_at;
-    let updated_at = result.updated_at;
-
-    if (bio == null) {
-      bio = "Developer";
+    let values = fieldOrder.map(k => result[k]);
+    let sql = `INSERT INTO user (${fieldOrder.join(
+      ","
+    )}) VALUES (${fieldOrder.map(e => "?").join(",")})`;
+    db.query(sql, values);
+  });
+  // User Repository Information API Process
+  request(repositoryOptions, function(error, responmse, data) {
+    if (error) {
+      throw error;
     }
-    db.query(
-      `INSERT INTO user (login, id, node_id, avatar_url, gravatar_id, url, html_url, followers_url, following_url, gists_url, starred_url, subscriptions_url, organizations_url, repos_url, events_url, received_events_url, type, site_admin, name, company, blog, location, email, hireable, bio, public_repos, public_gists, followers, following, created_at, updated_at) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)`,
-      [
-        nick,
-        id,
-        node_id,
-        avatar_url,
-        gravatar_id,
-        url,
-        html_url,
-        followers_url,
-        following_url,
-        gists_url,
-        starred_url,
-        subscriptions_url,
-        organizations_url,
-        repos_url,
-        events_url,
-        received_events_url,
-        type,
-        site_admin,
-        name,
-        company,
-        blog,
-        location,
-        email,
-        hireable,
-        bio,
-        public_repos,
-        public_gists,
-        followers,
-        following,
-        created_at,
-        updated_at
-      ]
-    );
+    let repositoryResult = JSON.parse(data);
   });
 
   res.redirect("/");
