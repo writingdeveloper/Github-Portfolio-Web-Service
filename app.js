678d16b9a83fda847edfb6e4467f3659ace974f0
Si Hyeong Lee <super2451894@gmail.com>
2018-12-07 23:59:59 +0900

    Update app.js
    test

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
-
+  
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
