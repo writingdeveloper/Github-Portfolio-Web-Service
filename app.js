401435aa835a7f9e2e120135499327655bd8b13d
Si Hyeong Lee <super2451894@gmail.com>
2019-01-04 14:35:47 +0900

    Update app.js
    - Unused Router Remove

--- app.js
+++ app.js
@@ -5,10 +5,10 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// var authRouter = require('./routes/auth');
 
 var app = express();
-  
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
@@ -22,7 +22,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// app.use('/auth', authRouter);
 
 
 // catch 404 and forward to error handler
