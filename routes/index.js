506fcd477a9c037f7e4d2b061f889aae62f69adf
Si Hyeong Lee <super2451894@gmail.com>
2019-01-17 17:26:51 +0900

    Update index.js
    - Remove Useless Comments
    - Fix bug If User name is not exist, save with github nickname

--- routes/index.js
+++ routes/index.js
@@ -32,91 +32,6 @@ const fieldOrder = [
   "bio"
 ];
 
-// let passport = require('passport');
-// let GitHubStrategy = require('passport-github').Strategy;
-
-// passport.use(new GitHubStrategy({
-//     clientID: '36eebe46884c9debc1f4',
-//     clientSecret: '73085d60a186fc60446bd49276968f1c3f0dc251',
-//     callbackURL: "https://expressme.herokuapp.com/auth/github/callback/"
-//   },
-//   function (accessToken, refreshToken, profile, cb) {
-//     console.log(accessToken);
-//     // console.log(profile);
-//     // Check Register
-//     db.query(`SELECT 0 FROM user WHERE id=?`, [profile.id], function (error, data) {
-//       if (error) {
-//         throw error;
-//       }
-//       // console.log(profile);
-//       // console.log(data);
-//       if (data.length > 0) {
-//         console.log('registered Member!!');
-//       } else {
-//         db.query(`INSERT INTO user (login, id, avatar_url, name, bio) VALUES (?, ?, ?, ?, ?)`, [profile.username, profile.id, profile._json.avatar_url, profile._json.name, profile._json.bio]);
-
-//         // Data
-//         let githubAPI = "https://api.github.com/users/";
-//         // User Information API Option Set
-//         let userOptions = {
-//           url: githubAPI + profile.username,
-//           headers: {
-//             "User-Agent": "request"
-//           }
-//         };
-//         // User Repository API Option Set
-//         let repositoryOptions = {
-//           url: githubAPI + profile.username + "/repos",
-//           headers: {
-//             "User-Agent": "request"
-//           }
-//         };
-//         // User Repository Information API Process
-//         request(repositoryOptions, function (error, response, data) {
-//           if (error) {
-//             throw error;
-//           }
-//           let result = JSON.parse(data);
-
-//           for (i = 0; i < result.length; i++) {
-//             // console.log(result[i]);
-//             let sid = shortid.generate();
-//             let githubid = result[i].owner.login;
-//             let name = result[i].name;
-//             let githuburl = result[i].html_url;
-//             let explanation = result[i].description;
-//             let created_at = result[i].created_at;
-//             let updated_at = result[i].updated_at;
-//             let sqlData = [sid, githubid, name, githuburl, explanation, created_at, updated_at];
-//             console.log(sqlData);
-//             let sql = `INSERT INTO Personal_Data (id, githubid, name, githuburl, explanation, pjdate1, pjdate2) VALUES (?,?,?,?,?,?,?)`;
-//             db.query(sql, sqlData);
-//           }
-//         })
-//       }
-//     })
-//     return cb(null, profile);
-//   }
-// ));
-
-// router.use(passport.initialize());
-// router.use(passport.session());
-
-// passport.serializeUser(function (user, cb) {
-//   cb(null, user.id);
-//   console.log('serializeUser', user.id);
-// });
-
-// passport.deserializeUser(function (obj, cb) {
-//   db.query(`SELECT * FROM user WHERE id=?`, [obj], function (error, data) {
-//     if (error) {
-//       throw error;
-//     }
-//     cb(null, data[0]);
-//     console.log('DeserializerUser', data[0]);
-//   })
-// });
-
 // Auth Router
 router.get('/auth/github',
   passport.authenticate('github'));
@@ -215,6 +130,9 @@ router.post("/user", function (req, res, next) {
     console.log(result);
     if (result.bio === null) {
       result.bio = '';
+    }
+    if (result.name === null) {
+      result.name = result.login;
     }
     let values = fieldOrder.map(k => result[k]);
     let sql = `INSERT INTO user (${fieldOrder.join(
