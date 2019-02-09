57d4a5656fe0f2a61a8425b149b6d68cad9d4683
Si Hyeong Lee <super2451894@gmail.com>
2019-02-09 10:37:56 +0900

    Update mypage.js
    - Mypage Remove Data Ajax Function Add

--- public/javascripts/mypage.js
+++ public/javascripts/mypage.js
@@ -4,28 +4,40 @@ $(document).ready(function () {
     // GET REQUEST
     $("#removeData").click(function (event) {
         event.preventDefault();
-        ajaxPost();
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            type: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!'
+        }).then((result) => {
+            if (result.value) {
+                ajaxPost();
+            }
+        })
     });
 
     function ajaxPost() {
         // PREPARE FORM DATA
-        let data = {
-            email: $("#email").val(),
-            phoneNumber: $("#phoneNumber").val(),
-            bio: $("#bio").val()
-        }
+        // let data = {
+        //     email: $("#email").val(),
+        //     phoneNumber: $("#phoneNumber").val(),
+        //     bio: $("#bio").val()
+        // }
         // DO POST
         $.ajax({
             type: "POST",
-            contentType: "application/json",
+            // contentType: "application/json",
             url: `/${userId}/admin/removeData`,
-            data: JSON.stringify(data),
-            dataType: 'json',
+            // data: JSON.stringify(data),
+            // dataType: 'json',
             success: function (data) {
-                console.log(JSON.stringify(data));
+                // console.log(JSON.stringify(data));
                 Swal.fire(
-                    'Modification completed',
-                    '',
+                    'Deleted!',
+                    'Your file has been deleted.',
                     'success'
                 )
             },
