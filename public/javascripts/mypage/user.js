d243b298f16442de8c0d8345ff8cfeeb7d17045b
Si Hyeong Lee <super2451894@gmail.com>
2019-03-07 17:21:19 +0900

    Create user.js
    - User page JS Add
    - Fix user information page

---
+++ public/javascripts/mypage/user.js
@@ -0,0 +1,30 @@
+/* User Page JS */
+
+/* POST User Data with Fetch API */
+function userInformationSend() {
+    let data = {
+        email: document.getElementById('email').value,
+        phoneNumber: document.getElementById('phoneNumber').value,
+        bio: document.getElementById('bio').value
+    }
+    fetch(`/${userId}/admin/submit`, {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: new Headers({
+            'Content-type': 'application/json'
+        })
+    }).then(res => res.json()).then(data => {
+        console.log(data);
+        Swal.fire(
+            'Modification completed',
+            '',
+            'success'
+        )
+    }).catch((err) => {
+        Swal.fire(
+            'ERROR?',
+            'SOMETHING IS NOT WOKRING',
+            `ERROR :${err}`
+        )
+    })
+}
