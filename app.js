f21d2c7fe02d6b32bb3d1828f68b5defed4ffc59
Si Hyeong Lee <super2451894@gmail.com>
2019-03-21 22:22:15 +0900

    Update app.js
    - Add Error Router

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 const db = require("./lib/db");
-var app = express();
+const app = express();
 
 app.io = require('socket.io')();
 
+const indexRouter = require('./routes/index.js');
+const portfolioRouter = require('./routes/portfolioData.js');
+const errorRouter=require('./routes/error.js');
 
-var indexRouter = require('./routes/index');
-var portfolioRouter = require('./routes/portfolioData');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -24,8 +25,11 @@ app.use(express.urlencoded({
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
 app.use('/', indexRouter);
 app.use('/', portfolioRouter);
+app.use('/', errorRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
