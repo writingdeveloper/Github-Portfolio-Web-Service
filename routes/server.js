beb51cfcff9570634985ffb4af725a656e2499ab
Si Hyeong Lee <super2451894@gmail.com>
2019-04-21 17:41:12 +0900

    Update server.js
    - Time Data Fix

--- routes/server.js
+++ routes/server.js
@@ -4,9 +4,10 @@ const request = require("request");
 const moment = require('moment-timezone');
 
 const telegramKey = process.env.telegramKey;
-let timeData = moment().tz("Asia/Seoul").format('YYYY-MM-DD HH:mm:ss');
+
 
 router.get(`/`, function (req, res, next) {
+    let timeData = moment().tz("Asia/Seoul").format('YYYY-MM-DD HH:mm:ss');
     let coreMessage = `MySQL Data BackUP Complete : ${timeData}`;
     request(`https://api.telegram.org/${telegramKey}/sendmessage?chat_id=550566016&text=${coreMessage}`)
     console.log('Finish');
