a459c704ccd9ed587b1aea69d181bf20f0746e20
Si Hyeong Lee <super2451894@gmail.com>
2019-08-09 20:49:53 +0900

    Create findUser.js
    - Add Find User JavaScript

---
+++ public/javascripts/findUser.js
@@ -0,0 +1,23 @@
+let pageNumber;
+
+function loadMoreUsers() {
+    pageNumber = document.getElementsByClassName('col-md-3').length;
+    console.log(pageNumber);
+    fetch(`/find-users/moreuser`, {
+        method: 'GET'
+
+    }).then(res => res.json()).then(data => {
+        console.log(data);
+        for (let i = 0; i < data.length; i++) {
+            let newDiv = document.createElement("div");
+            newDiv.className = 'col-md-3'
+            newDiv.innerHTML = `
+            <div class="card mb-3 box-shadow"><img class="card-img-top" src="${data[i].avatarUrl}"><div class="card-body"><p class="card-name">${data[i].loginId}</p><p class="card-text">${data[i].bio}</p><div class="d-flex justify-content-between align-items-center"><div class="btn-group"><button class="btn btn-sm btn-outline-secondary" type="button">View</button><button class="btn btn-sm btn-outline-secondary" type="button">Edit</button></div><small class="text-muted">9 mins</small></div></div></div>
+            `;
+            document.getElementById("additional").appendChild(newDiv);
+
+        }
+    }).catch((err) => {
+        console.log(err)
+    });
+}
