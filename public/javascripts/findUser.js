ddf1e204d6b001a3235468333674b07b0dd05524
Si Hyeong Lee <super2451894@gmail.com>
2019-08-19 19:41:50 +0900

    Find Users page Update
    - Fix Find user Router
    - Fix Find user template
    - Fix Find user JavaScript
    - Add Search Function

--- public/javascripts/findUser.js
+++ public/javascripts/findUser.js
@@ -1,35 +1,112 @@
 /* Find Users Page */
 
-let pageNumber;
+/* Search Users Button Function */
+function searchUsers() {
+  let queryString = document.getElementById("searchUsers").value;
+  let removeElements = elms => elms.forEach(el => el.remove());
+  console.log(queryString);
 
-/* Load More Users Button Function */
-function loadMoreUsers() {
-    pageNumber = document.getElementsByClassName('col-md-3').length;
-    console.log(pageNumber);
-    fetch(`/find-users/moreuser/${pageNumber}`, {
-        method: 'GET',
-    }).then(res => res.json()).then(data => {
+  if (['', '{', '}', '{}'].includes(queryString)) {
+    removeElements(document.querySelectorAll(".col-md-3"));
+    removeElements(document.querySelectorAll("#loadMoreUsers"));
+    removeElements(document.querySelectorAll(".alert"));
+    let newDiv = document.createElement("div");
+    newDiv.innerHTML = `<div class="alert alert-danger" role="alert">
+          Enter skills or some information!
+        </div>`
+    document.getElementById("container").appendChild(newDiv);
+  } else {
+    fetch(`/find-users/${queryString}`, { // Request Search Data to DB server
+        method: "GET"
+      })
+      .then(res => res.json())
+      .then(data => {
         console.log(data);
-        if (data === 'NODATA') { // End of the Profile Data
+        if (data === "NODATA") {
+          // End of the Profile Data
+          removeElements(document.querySelectorAll(".col-md-3"));
+          removeElements(document.querySelectorAll("#loadMoreUsers"));
+          removeElements(document.querySelectorAll(".alert"));
+          let newDiv = document.createElement("div");
+          newDiv.innerHTML = `<div class="alert alert-danger" role="alert">
+          No Result
+        </div>`
+          document.getElementById("container").appendChild(newDiv);
         } else {
-            for (let i = 0; i < data.length; i++) {
-                let newDiv = document.createElement("div");
-                newDiv.className = 'col-md-3'
-                newDiv.innerHTML = `
-            <div class="card mb-3 box-shadow"><img class="card-img-top" src="${data[i].avatarUrl}">
+
+          // Remove Exist Profile Data
+          removeElements(document.querySelectorAll(".col-md-3"));
+          removeElements(document.querySelectorAll(".alert"));
+
+          for (let i = 0; i < data.length; i++) {
+            let newDiv = document.createElement("div");
+            newDiv.className = "col-md-3";
+            // Append new profile HTML
+            newDiv.innerHTML = `
+            <div class="card mb-3 box-shadow"><img class="card-img-top" src="${
+              data[i].avatarUrl
+            }">
             <div class="card-body">
             <p class="card-name">${data[i].loginId}</p>
             <p class="card-text">${data[i].bio}</p>
             <div class="d-flex justify-content-between align-items-center">
             <div class="btn-group">
-            <button class="btn btn-sm btn-outline-secondary" type="button" onClick="location.href='${data[i].loginId}'">View</button>
+            <button class="btn btn-sm btn-outline-secondary" type="button" onClick="location.href='${
+              data[i].loginId
+            }'">View</button>
+            <button class="btn btn-sm btn-outline-secondary" type="button" onClick="location.href='${
+              data[i].loginId
+            }/contact'">Contact</button></div>
+            <small class="text-muted">9 mins</small></div></div></div>
+            `;
+            document.getElementById("additional").appendChild(newDiv);
+          }
+        }
+      })
+
+      .catch(err => {
+        console.log(err); // Log Error
+      });
+  }
+}
+
+/* Load More Users Button Function */
+function loadMoreUsers() {
+  let pageNumber = document.getElementsByClassName("col-md-3").length;
+  console.log(pageNumber);
+  fetch(`/find-users/moreuser/${pageNumber}`, {
+      method: "GET"
+    })
+    .then(res => res.json())
+    .then(data => {
+      console.log(data);
+      if (data === "NODATA") {
+        // End of the Profile Data
+      } else {
+        for (let i = 0; i < data.length; i++) {
+          let newDiv = document.createElement("div");
+          newDiv.className = "col-md-3";
+          // Append new profile HTML
+          newDiv.innerHTML = `
+            <div class="card mb-3 box-shadow"><img class="card-img-top" src="${
+              data[i].avatarUrl
+            }">
+            <div class="card-body">
+            <p class="card-name">${data[i].loginId}</p>
+            <p class="card-text">${data[i].bio}</p>
+            <div class="d-flex justify-content-between align-items-center">
+            <div class="btn-group">
+            <button class="btn btn-sm btn-outline-secondary" type="button" onClick="location.href='${
+              data[i].loginId
+            }'">View</button>
             <button class="btn btn-sm btn-outline-secondary" type="button">Edit</button></div>
             <small class="text-muted">9 mins</small></div></div></div>
             `;
-                document.getElementById("additional").appendChild(newDiv);
-            }
+          document.getElementById("additional").appendChild(newDiv);
         }
-    }).catch((err) => {
-        console.log(err) // Log Error
+      }
+    })
+    .catch(err => {
+      console.log(err); // Log Error
     });
 }
--- routes/findUser.js
+++ routes/findUser.js
@@ -1,62 +1,86 @@
 const express = require("express");
 const router = express.Router();
+const {
+  promisify
+} = require("util");
 
 /* Import Database Settings */
 const db = require("../lib/db");
 
 router.get("/find-users", function (req, res, next) {
-  db.query(`SELECT * FROM user ORDER BY registerDate DESC LIMIT 0, 8`, function (error, data) { // GET Data sort with register_time and get 6 Profile
-    // Log Error
+  db.query(`SELECT * FROM user ORDER BY registerDate DESC LIMIT 0, 8`, function (
+    error,
+    data
+  ) {
     if (error) {
       console.log(error);
     }
-    let url = ''
-    if (req.user) {
-      url = req.user.loginId
-    } else {
-      url = '';
-    }
-    // Main Page BIO NULL Check
+    // data NULL check
     data.forEach(results => {
       if (results.bio === null) {
-        results.bio = 'NO BIO';
+        results.bio = "NO BIO";
       }
-    })
+    });
     res.render("find-users", {
-      dataarray: data,
-      _user: req.user,
-      url: url
-    })
+      dataarray: data
+    });
   });
 });
 
+/* Search User Router */
+router.get(`/find-users/:queryString`, async function (req, res, next) {
+  const query = promisify(db.query).bind(db);
+  let queryString = req.params.queryString;
+  const data = await query(
+    `SELECT distinct userId from project WHERE UPPER(keyword) LIKE UPPER("%${queryString}%")`
+  );
+  if (data == '' || data == []) {
+    res.json('NODATA');
+  } else {
+    let userArray = [];
+    for (let i = 0; i < data.length; i++) {
+      const userData = await query(
+        `SELECT * FROM user WHERE loginId='${data[i].userId}'`
+      );
+      if (!userData) {
+        console.log("error");
+
+      } else {
+        // data NULL check
+        userData.forEach(results => {
+          if (results.bio === null) {
+            results.bio = "NO BIO";
+          }
+        });
+        userArray.push(userData[0]);
+      }
+    }
+    console.log(userArray)
+    res.json(userArray); // Return search data
+  }
+});
+
+/* More Users Button Router */
 router.get(`/find-users/moreuser/:page`, function (req, res, next) {
-  db.query(`SELECT * FROM user ORDER BY registerDate DESC LIMIT ${req.params.page}, 8`, function (error, data) {
-    console.log(data.length);
-    if (data.length === 0) {
-      console.log('No more data')
-      res.json('NODATA');
-    } else {
-      // page += 8;
-      // Log Error
+  db.query(
+    `SELECT * FROM user ORDER BY registerDate DESC LIMIT ${req.params.page}, 8`,
+    function (error, data) {
       if (error) {
         console.log(error);
       }
-      let url = ''
-      if (req.user) {
-        url = req.user.loginId
+      if (data.length === 0) {
+        res.json("NODATA"); // If no more data return string "NODATA" to process in client script
       } else {
-        url = '';
+        // data NULL check
+        data.forEach(results => {
+          if (results.bio === null) {
+            results.bio = "NO BIO";
+          }
+        });
+        res.json(data); // Return more user data
       }
-      // Main Page BIO NULL Check
-      data.forEach(results => {
-        if (results.bio === null) {
-          results.bio = 'NO BIO';
-        }
-      })
-      res.json(data);
     }
-  })
-})
+  );
+});
 
 module.exports = router;
