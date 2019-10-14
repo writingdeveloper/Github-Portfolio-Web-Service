227f89d04782ea6af48e9fc73df28a555336c0f5
Si Hyeong Lee <super2451894@gmail.com>
2019-10-14 22:39:20 +0900

    Update index.js
    - Remove Sitemap App

--- routes/index.js
+++ routes/index.js
@@ -4,71 +4,33 @@ const bodyParser = require("body-parser");
 const path = require("path");
 const request = require("request");
 const shortid = require("shortid");
-let session = require('express-session');
-let FileStore = require('session-file-store')(session);
-
-/* Sitemap */
-const {
-  SitemapStream,
-  streamToPromise
-} = require('sitemap')
-const {
-  createGzip
-} = require('zlib')
-let sitemap;
-
-router.get('/sitemap.xml', function (req, res) {
-  res.header('Content-Type', 'application/xml');
-  res.header('Content-Encoding', 'gzip');
-  // if we have a cached entry send it
-  if (sitemap) {
-    res.send(sitemap)
-    return
-  }
-  try {
-    const smStream = new SitemapStream({
-        hostname: 'http://127.0.0.1:3000/'
-      })
-      .pipe(createGzip())
-
-    smStream.write({
-      url: '/',
-      changefreq: 'daily',
-      priority: 0.3
-    })
-
-    // cache the response
-    streamToPromise(gzippedStream).then(sm => sitemap = sm)
-    // stream the response
-    gzippedStream.pipe(res).on('error', (e) => {
-      throw e
-    })
-  } catch (e) {
-    console.error(e)
-    res.status(500).end()
-  }
-})
+let session = require("express-session");
+let FileStore = require("session-file-store")(session);
 
 /* Redirect http to https */
-router.get('*', function (req, res, next) {
-  if (req.headers['x-forwarded-proto'] != 'https' && process.env.NODE_ENV === 'production')
-    res.redirect('https://' + req.hostname + req.url)
-  else
-    next() /* Continue to other routes if we're not redirecting */
+router.get("*", function (req, res, next) {
+  if (
+    req.headers["x-forwarded-proto"] != "https" &&
+    process.env.NODE_ENV === "production"
+  )
+    res.redirect("https://" + req.hostname + req.url);
+  else next(); /* Continue to other routes if we're not redirecting */
 });
 
-router.use(session({
-  secret: process.env.session_secret || 'Hello World',
-  resave: false,
-  saveUninitialized: true,
-  store: new FileStore(),
-  cookie: {
-    secure: false,
-    // maxAge: new Date(Date.now() + 3600000)
-    maxAge: 24000 * 60 * 60
-  },
-  key: 'connect.sid'
-}));
+router.use(
+  session({
+    secret: process.env.session_secret || "Hello World",
+    resave: false,
+    saveUninitialized: true,
+    store: new FileStore(),
+    cookie: {
+      secure: false,
+      // maxAge: new Date(Date.now() + 3600000)
+      maxAge: 24000 * 60 * 60
+    },
+    key: "connect.sid"
+  })
+);
 
 // Favicon Server Dependency
 let favicon = require("serve-favicon");
@@ -77,35 +39,40 @@ router.use(favicon(path.join(__dirname, "../public/images", "favicon.ico")));
 /* Import Database Settings */
 const db = require("../lib/db");
 /* Import Authentication Setting (Passport.js) */
-const passport = require('../lib/passport')(router, db, request, shortid);
+const passport = require("../lib/passport")(router, db, request, shortid);
 
 /* Github Auth Router */
-router.get('/auth/github',
-  passport.authenticate('github'));
+router.get("/auth/github", passport.authenticate("github"));
 /* Github Auth Callback Router */
-router.get('/auth/github/callback',
-  passport.authenticate('github', {
-    failureRedirect: '/auth/login'
+router.get(
+  "/auth/github/callback",
+  passport.authenticate("github", {
+    failureRedirect: "/auth/login"
   }),
   function (req, res) {
     // Successful authentication, redirect home.
-    console.log('SUCESS!!', req.user);
+    console.log("SUCESS!!", req.user);
     res.redirect(`/${req.user.username}`);
-  });
+  }
+);
 
 /* Google Auth Router */
-router.get('/auth/google',
-  passport.authenticate('google', {
-    scope: ['profile']
-  }));
-router.get('/auth/google/callback',
-  passport.authenticate('google', {
-    failureRedirect: '/login'
+router.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["profile"]
+  })
+);
+router.get(
+  "/auth/google/callback",
+  passport.authenticate("google", {
+    failureRedirect: "/login"
   }),
   function (req, res) {
     // Successful authentication, redirect home.
     res.redirect(`/`);
-  });
+  }
+);
 
 /* Login Page Router */
 router.get(`/auth/login`, function (req, res, next) {
@@ -131,41 +98,60 @@ router.use(
 
 /* SiteMap */
 router.get(`/sitemap/:page`, function (req, res, next) {
-  let pageNumber = String(req.params.page) + '00';
-  console.log(pageNumber)
-  // db.query(`SELECT * FROM user WHERE loginId=?`, [userId], function (error, data) {
-  db.query(`SELECT * FROM user order by displayId*1 LIMIT ${pageNumber}, 100`, function (error, data) {
-    if (error) {
-      throw error;
+  let pageNumber = (Number(req.params.page) * 1000); // Current Page Number
+  let nextPage = (Number(req.params.page) + 1) * 1000; // Next Page Number
+  let pageResult; // Page Result
+  db.query(
+    `SELECT * FROM user order by displayId*1 LIMIT ?, 1000`,
+    [Number(pageNumber)],
+    function (error, data) {
+      if (error) {
+        throw error;
+      }
+      // console.log(data);
+      db.query(`SELECT * FROM user order by displayId*1 LIMIT ?, 1000`, [Number(nextPage)], function (error, pageData) {
+        if (error) {
+          throw error;
+        }
+        if (pageData.length === 0) { // If no more data in next Page
+          pageResult = 'NODATA'
+        } else {
+          pageResult = (Number(req.params.page) + 1) // More Data Exists return next page URL
+        }
+        res.render("sitemap", {
+          dataarray: data,
+          pageNumber: pageNumber,
+          pageResult
+        })
+      });
     }
-    // console.log(data);
-
-    res.render("sitemap", {
-      dataarray: data
-    })
-  });
+  );
 });
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
   // Main page Profile Data Process
-  db.query(`SELECT * FROM user ORDER BY registerDate DESC LIMIT 5`, function (error, data) { // GET Data sort with register_time and get 6 Profile
+  db.query(`SELECT * FROM user ORDER BY registerDate DESC LIMIT 5`, function (
+    error,
+    data
+  ) {
+    // GET Data sort with register_time and get 6 Profile
     // Log Error
     if (error) {
       console.log(error);
     }
-    let url = ''
+    let url = "";
     if (req.user) {
-      url = req.user.loginId
+      url = req.user.loginId;
     } else {
-      url = '';
+      url = "";
     }
     // Main Page BIO NULL Check
     data.forEach(results => {
       if (results.bio === null) {
-        results.bio = 'NO BIO';
+        results.bio = "NO BIO";
       }
-    })
+    });
     res.render("main", {
       dataarray: data,
       _user: req.user,
@@ -196,7 +182,7 @@ router.post("/user", function (req, res, next) {
   // User Information API Process
   request(userOptions, function (error, response, data) {
     if (error) {
-      throw (`Error from index.js User Information API Process ${error}`);
+      throw `Error from index.js User Information API Process ${error}`;
     }
     // result have JSON User Data
     let result = JSON.parse(data);
@@ -204,13 +190,16 @@ router.post("/user", function (req, res, next) {
     if (result.name === null) {
       result.name = result.login;
     }
-    db.query(`INSERT INTO user (loginId, displayId, avatarUrl, name, bio, registerType) VALUES (?,?,?,?,?,'Gtihub')`, [result.login, result.id, result.avatar_url, result.name, result.bio]); // User Information INSERT SQL
+    db.query(
+      `INSERT INTO user (loginId, displayId, avatarUrl, name, bio, registerType) VALUES (?,?,?,?,?,'Gtihub')`,
+      [result.login, result.id, result.avatar_url, result.name, result.bio]
+    ); // User Information INSERT SQL
   });
 
   // User Repository Information API Process
   request(repositoryOptions, function (error, response, data) {
     if (error) {
-      throw (`Error from index.js User Repository Information API Process ${error}`);
+      throw `Error from index.js User Repository Information API Process ${error}`;
     }
     let result = JSON.parse(data);
     for (i = 0; i < result.length; i++) {
@@ -221,7 +210,15 @@ router.post("/user", function (req, res, next) {
       let summary = result[i].description;
       let projectDate1 = result[i].created_at;
       let projectDate2 = result[i].updated_at;
-      let sqlData = [sid, userId, projectName, githubUrl, summary, projectDate1, projectDate2];
+      let sqlData = [
+        sid,
+        userId,
+        projectName,
+        githubUrl,
+        summary,
+        projectDate1,
+        projectDate2
+      ];
       console.log(sqlData);
       let sql = `INSERT INTO project (sid, userId, projectName, githubUrl, summary, projectDate1, projectDate2) VALUES (?,?,?,?,?,?,?)`;
       db.query(sql, sqlData);
@@ -233,7 +230,11 @@ router.post("/user", function (req, res, next) {
 /* Get newly Data */
 router.get(`/getAllData`, function (req, res, next) {
   // Main page Profile Data Process
-  db.query(`SELECT * FROM project WHERE keyword IS NULL`, function (error, data) { // GET Data sort with register_time and get 6 Profile
+  db.query(`SELECT * FROM project WHERE keyword IS NULL`, function (
+    error,
+    data
+  ) {
+    // GET Data sort with register_time and get 6 Profile
     // Log Error
     if (error) {
       console.log(error);
@@ -242,7 +243,8 @@ router.get(`/getAllData`, function (req, res, next) {
     function timer(ms) {
       return new Promise(res => setTimeout(res, ms));
     }
-    async function load() { // We need to wrap the loop into an async function for this to work
+    async function load() {
+      // We need to wrap the loop into an async function for this to work
       for (let i = 0; i < data.length; i++) {
         let getDataOption = {
           url: `https://api.github.com/repos/${data[i].userId}/${data[i].projectName}/languages`,
@@ -255,13 +257,15 @@ router.get(`/getAllData`, function (req, res, next) {
           if (err) {
             console.log(err);
           }
-          db.query(`UPDATE project SET keyword='${body}' WHERE userId='${data[i].userId}' AND projectName='${data[i].projectName}'`)
+          db.query(
+            `UPDATE project SET keyword='${body}' WHERE userId='${data[i].userId}' AND projectName='${data[i].projectName}'`
+          );
         });
         await timer(3000); // then the created Promise can be awaited
       }
     }
     load();
-    res.end()
+    res.end();
   });
 });
 
