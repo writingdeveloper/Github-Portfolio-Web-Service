f16980182cd58d82edb78f44d0bbc2d5adf8429f
Si Hyeong Lee <super2451894@gmail.com>
2020-09-22 15:34:05 +0900

    Upload yml files
    - Upload yml files

--- start.sh
+++ start.sh
@@ -1,3 +1,17 @@
+# # /start.sh
+# # nvm에 대한 환경변수를 설정하는 것임.
+# export NVM_DIR="$HOME/.nvm"
+# [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+# [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+
+# cd $PROJECT_ROOT
+
+# # 원래 node 프로세스 종료
+# sudo pkill -f node
+# nohup npm start >/home/project/logs 2>&1 </home/project/errors &
+
+# # `ps -ef | grep 'node ./bin/www' | awk '{print $2}'`
+
 # /start.sh
 # nvm에 대한 환경변수를 설정하는 것임.
 export NVM_DIR="$HOME/.nvm"
@@ -7,7 +21,5 @@ export NVM_DIR="$HOME/.nvm"
 cd $PROJECT_ROOT
 
 # 원래 node 프로세스 종료
-sudo pkill -f node
-nohup npm start >/home/project/logs 2>&1 </home/project/errors &
-
-# `ps -ef | grep 'node ./bin/www' | awk '{print $2}'`
+sudo kill -9 `ps -ef | grep 'node ./bin/www' | awk '{print $2}'`
+nohup npm start >/home/ubuntu/logs 2>&1 </home/ubuntu/errors &
