54eeae6216e03f9924300ee821846e595573e50e
Si Hyeong Lee <super2451894@gmail.com>
2020-10-07 21:50:39 +0900

    Update start.sh
    - kill node script fix

--- start.sh
+++ start.sh
@@ -21,5 +21,5 @@ export NVM_DIR="$HOME/.nvm"
 cd $PROJECT_ROOT
 
 # 원래 node 프로세스 종료
-sudo pkill -f node `ps -ef | grep 'node ./bin/www' | awk '{print $2}'`
+sudo pkill -f node `grep 'node ./bin/www' | awk '{print $2}'`
 nohup npm start >/home/project/logs 2>&1 </home/project/errors &
