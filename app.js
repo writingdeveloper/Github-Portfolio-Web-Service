81a65c470236d6d2a98fe9b39b95c199aab74a99
Si Hyeong Lee <super2451894@gmail.com>
2020-10-30 10:53:28 +0900

    Update app.js
    - request limit fix

--- app.js
+++ app.js
@@ -14,8 +14,8 @@ If the environment variable fails to load, run the node app with `node -r dotenv
 */
 
 const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100 // limit each IP to 100 requests per windowMs
+  windowMs: 1 * 60 * 1000, // 15 minutes
+  max: 20 // limit each IP to 100 requests per windowMs
 });
 app.use(limiter);
 
