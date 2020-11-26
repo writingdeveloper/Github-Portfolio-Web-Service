451d23c18f264b6f4e47390a1f2edb4225225574
Si Hyeong Lee <super2451894@gmail.com>
2020-11-26 21:14:35 +0900

    Add cd command
    - Add cd command

--- scripts/start.sh
+++ scripts/start.sh
@@ -4,7 +4,7 @@
 # [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 # [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 
-cd $PROJECT_ROOT
+# cd $PROJECT_ROOT
 
 # # 원래 node 프로세스 종료
 # sudo pkill -f node
@@ -19,6 +19,7 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 
 # 원래 node 프로세스 종료
+cd /home/projects/build
 sudo npm start
 # sudo pkill -f node `grep 'node ./bin/www' | awk '{print $2}'`
 # sudo pm2 stop app
